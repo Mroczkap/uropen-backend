e333fab27991e42d0dafb0918824999a4f73f487
Mroczkap <pawel.mroczka@neti-soft.com>
2025-12-04 16:12:39 +0100

    update cors

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -16,7 +16,9 @@ const allowedOrigins = [
     'https://uropen.vercel.app/',
     'https://uropen-front-mroczkaps-projects.vercel.app/',
     'https://uropen.vercel.app',
-      'https://lks-stobierna.vercel.app',
+    'https://lks-stobierna.vercel.app',
+    'https://ts-app-chi.vercel.app/l',
+
 
 ];
 
